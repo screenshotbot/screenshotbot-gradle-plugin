477ef564b09d89da15e96d603eac44508cdaceb2
Arnold Noronha <arnold@tdrhq.com>
2023-11-11 16:27:40 -0500

    Sets up the initial record tasks
    Differential Revision: https://phabricator.tdrhq.com/D8182

---
+++ plugin/src/main/java/io/screenshotbot/gradle/plugin/RecordPaparazziTask.java
@@ -0,0 +1,6 @@
+package io.screenshotbot.gradle.plugin;
+
+import org.gradle.api.DefaultTask;
+
+public class RecordPaparazziTask extends DefaultTask {
+}
--- plugin/src/main/java/io/screenshotbot/gradle/plugin/ScreenshotbotPlugin.java
+++ plugin/src/main/java/io/screenshotbot/gradle/plugin/ScreenshotbotPlugin.java
@@ -1,6 +1,10 @@
 package io.screenshotbot.gradle.plugin;
 
+import groovy.lang.Closure;
 import org.gradle.api.*;
+import org.gradle.api.tasks.TaskContainer;
+
+import static org.gradle.api.plugins.JavaBasePlugin.VERIFICATION_GROUP;
 
 public class ScreenshotbotPlugin implements Plugin<Project> {
     public static class Extension {
@@ -9,5 +13,32 @@ public class ScreenshotbotPlugin implements Plugin<Project> {
     @Override
     public void apply(Project target) {
         target.getExtensions().create("screenshotbot", Extension.class);
+
+        applyPaparazziTasks(target);
+
+    }
+
+    private void applyPaparazziTasks(Project project) {
+        Action<Plugin> action = new Action<Plugin>() {
+            @Override
+            public void execute(Plugin plugin) {
+                project.afterEvaluate((project) -> {
+                    TaskContainer tasks = project.getTasks();
+                    tasks.stream().forEach((task) -> {
+
+                        if (task.getName().startsWith("recordPaparazzi")) {
+                            tasks.register(task.getName() + "Screenshotbot",
+                                            RecordPaparazziTask.class)
+                                    .configure((it) -> {
+                                        it.setGroup(VERIFICATION_GROUP);
+                                        it.setDescription("Records paparazzi screenshots into Screenshotbot");
+                                    });
+                        }
+                    });
+                });
+            }
+        };
+        project.getPlugins().withId("app.cash.paparazzi", action);
+
     }
 }
