282733fb3e76dacdd6354cc742521aa554e6a9c3
Arnold Noronha <arnold@tdrhq.com>
2023-11-11 14:18:44 -0500

    The build goes through
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:

---
+++ plugin/src/main/java/io/screenshotbot/gradle/plugin/ScreenshotbotPlugin.java
@@ -0,0 +1,10 @@
+package io.screenshotbot.gradle.plugin;
+
+import org.gradle.api.*;
+
+public class ScreenshotbotPlugin implements Plugin<Project> {
+    @Override
+    public void apply(Project target) {
+
+    }
+}
