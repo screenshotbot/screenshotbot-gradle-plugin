fa23a71f7843eeba194b4c76529ac1126d278069
Arnold Noronha <arnold@tdrhq.com>
2023-11-11 14:26:37 -0500

    More boilerplate: extensions, and publishToMavenLocal
    Differential Revision: https://phabricator.tdrhq.com/D8180

--- plugin/src/main/java/io/screenshotbot/gradle/plugin/ScreenshotbotPlugin.java
+++ plugin/src/main/java/io/screenshotbot/gradle/plugin/ScreenshotbotPlugin.java
@@ -3,8 +3,11 @@ package io.screenshotbot.gradle.plugin;
 import org.gradle.api.*;
 
 public class ScreenshotbotPlugin implements Plugin<Project> {
+    public static class Extension {
+        String hostname = "https://api.screenshotbot.io";
+    }
     @Override
     public void apply(Project target) {
-
+        target.getExtensions().create("screenshotbot", Extension.class);
     }
 }
