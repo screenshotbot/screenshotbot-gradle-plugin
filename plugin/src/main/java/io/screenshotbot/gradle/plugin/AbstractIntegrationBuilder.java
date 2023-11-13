cb30c704e3dfbc4bb4e120b3eba29389af4dfb1b
Arnold Noronha <arnold@tdrhq.com>
2023-11-13 15:45:45 -0500

    Refactor to support multiple different plugin frameworks
    Summary:
    ...
    
    ...
    
    ...
    
    ...
    
    ...
    
    ...
    
    ...
    
    ...
    
    ...
    
    ...
    
    ...
    
    ...
    
    Differential Revision: https://phabricator.tdrhq.com/D8214

---
+++ plugin/src/main/java/io/screenshotbot/gradle/plugin/AbstractIntegrationBuilder.java
@@ -0,0 +1,157 @@
+package io.screenshotbot.gradle.plugin;
+
+import org.gradle.api.Action;
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+import org.gradle.api.Task;
+import org.gradle.api.file.Directory;
+import org.gradle.api.tasks.TaskContainer;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static java.nio.file.StandardCopyOption.ATOMIC_MOVE;
+
+public abstract class AbstractIntegrationBuilder {
+    protected void safeDelete(File file) {
+        if (!file.delete()) {
+            throw new RuntimeException("Could not delete: " + file);
+        }
+    }
+
+    protected void restoreDir(Directory snapshotsDir) {
+        String backupDir = snapshotsDir.getAsFile().toString() + "-screenshotbot-backup";
+
+        deleteDirectory(snapshotsDir.getAsFile());
+
+        if (new File(backupDir).exists()) {
+            try {
+                Files.move(Path.of(backupDir), snapshotsDir.getAsFile().toPath());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private void deleteDirectory(File asFile) {
+        for (File file : asFile.listFiles()) {
+            // I want to avoid having bugs here that accidently delete outside of this directory, and
+            // I don't want dependencies, so I'll hardcode some of the directories I'm allowed to recurse
+            // into
+            if (file.isDirectory() && file.getName().equals("images")) {
+                deleteDirectory(file);
+            } else {
+                safeDelete(file);
+            }
+        }
+        safeDelete(asFile);
+    }
+
+    protected void backupDir(Directory dir) {
+        try {
+            String dest = dir.getAsFile().toString() + "-screenshotbot-backup";
+            // This isn't ideal.. but if the directory already exists, let's assume it was a previous backup, so we should restore it.
+            if (!new File(dest).exists()) {
+                Files.move(dir.getAsFile().toPath(),
+                        Path.of(dest), ATOMIC_MOVE);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    protected void prepareTask(Task task, Project project, String mode) {
+        TaskContainer tasks = project.getTasks();
+
+        String taskName = generateTaskName(task, mode);
+        String backupSnapshots = taskName + "BackupSnapshots";
+        String restoreSnapshots = taskName + "RestoreSnapshots";
+        String uploadSnapshots = taskName + "UploadSnapshots";
+
+        task.mustRunAfter(backupSnapshots);
+        tasks.register(taskName,
+                        RecordPaparazziTask.class)
+                .configure((it) -> {
+                    it.setGroup("Screenshotbot");
+                    it.setDescription("Records " + getPluginName() + " screenshots into Screenshotbot");
+                    it.dependsOn(task.getName());
+                    it.dependsOn(backupSnapshots);
+                    it.dependsOn(uploadSnapshots);
+                    it.dependsOn(restoreSnapshots);
+
+                });
+        tasks.register(backupSnapshots).configure((it) -> {
+            it.doFirst((it2) -> {
+                backupDir(getSnapshotsDir(project));
+            });
+        });
+
+        tasks.register(uploadSnapshots, UploadScreenshotsTask.class)
+                .configure((it) -> {
+                    it.directory = getImagesDirectory(project);
+                    it.channel = project.getPath();
+                    it.mode = mode;
+
+                    it.mustRunAfter(task.getName());
+                    it.doFirst((innerTask) -> {
+
+                    });
+                });
+
+        tasks.register(restoreSnapshots)
+                .configure((it) -> {
+                    it.mustRunAfter(uploadSnapshots);
+                    it.doFirst((innerTask) -> {
+                        restoreDir(getSnapshotsDir(project));
+                    });
+                });
+    }
+
+    public void apply(Project project) {
+        Action<Plugin> action = new Action<Plugin>() {
+            @Override
+            public void execute(Plugin plugin) {
+                project.afterEvaluate((project) -> {
+                    TaskContainer tasks = project.getTasks();
+                    tasks.stream().forEach((task) -> {
+
+                        if (isApplicableTask(task)) {
+                            prepareTask(task, project, "record");
+                            prepareTask(task, project, "verify");
+                            prepareTask(task, project, "ci");
+                        }
+
+                    });
+                });
+            }
+        };
+        project.getPlugins().withId(getPluginId(), action);
+
+    }
+
+    @NotNull
+    protected abstract String getPluginId();
+
+    protected abstract boolean isApplicableTask(Task task);
+
+    /*
+     * This might be the same as getSnapshotsDir(), but for example with Paparazzi the
+     * snapshots dir is the directory we're backing up, but the images directory is snapshots-dir/images.
+     */
+    @NotNull
+    protected File getImagesDirectory(Project project) {
+        return getSnapshotsDir(project).getAsFile();
+    }
+
+    @NotNull
+    protected abstract String getPluginName();
+
+    @NotNull
+    protected abstract String generateTaskName(Task task, String mode);
+
+    @NotNull
+    protected abstract Directory getSnapshotsDir(Project project);
+}
---
+++ plugin/src/main/java/io/screenshotbot/gradle/plugin/PaparazziIntegrationBuilder.java
@@ -0,0 +1,62 @@
+package io.screenshotbot.gradle.plugin;
+
+import org.gradle.api.*;
+import org.gradle.api.file.Directory;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+
+public class PaparazziIntegrationBuilder extends AbstractIntegrationBuilder {
+
+    @NotNull
+    @Override
+    protected String getPluginId() {
+        return "app.cash.paparazzi";
+    }
+
+    @Override
+    protected boolean isApplicableTask(Task task) {
+        return task.getName().startsWith("recordPaparazzi");
+    }
+
+
+    /*
+     * This might be the same as getSnapshotsDir(), but for example with Paparazzi the 
+     * snapshots dir is the directory we're backing up, but the images directory is snapshots-dir/images. 
+     */
+    @NotNull
+    @Override
+    protected File getImagesDirectory(Project project) {
+        return new File(getSnapshotsDir(project).getAsFile(), "images");
+    }
+
+    @NotNull
+    @Override
+    protected String getPluginName() {
+        return "Paparazzi";
+    }
+
+    @NotNull
+    @Override
+    protected String generateTaskName(Task task, String mode) {
+        String taskName = task.getName();
+        String nameWithoutPrefix = task.getName().substring("record".length());
+        if (mode.equals("verify")) {
+            taskName = "verify" + nameWithoutPrefix + "Screenshotbot";
+        } else if (mode.equals("ci")) {
+            taskName = "recordAndVerify" + nameWithoutPrefix + "ScreenshotbotCI";
+        } else {
+            taskName += "Screenshotbot"; // For record
+        }
+
+        return taskName;
+    }
+
+
+    @NotNull
+    @Override
+    protected Directory getSnapshotsDir(Project project) {
+        return project.getLayout().getProjectDirectory().dir("src/test/snapshots");
+    }
+
+}
