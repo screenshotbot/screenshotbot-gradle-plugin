3e30d6a920b73fdbcbfd67443d70f60cc4b8634d
Arnold Noronha <arnold@tdrhq.com>
2023-11-13 15:30:57 -0500

    ...

--- plugin/src/main/java/io/screenshotbot/gradle/plugin/AbstractIntegrationBuilder.java
+++ plugin/src/main/java/io/screenshotbot/gradle/plugin/AbstractIntegrationBuilder.java
@@ -1,11 +1,59 @@
 package io.screenshotbot.gradle.plugin;
 
+import org.gradle.api.file.Directory;
+
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static java.nio.file.StandardCopyOption.ATOMIC_MOVE;
 
 public class AbstractIntegrationBuilder {
     protected void safeDelete(File file) {
         if (!file.delete()) {
             throw new RuntimeException("Could not delete: " + file);
+        }
+    }
+
+    protected void restoreDir(Directory snapshotsDir) {
+        String backupDir = snapshotsDir.getAsFile().toString() + "-screenshotbot-backup";
+
+        deleteDirectory(snapshotsDir.getAsFile());
+
+        if (new File(backupDir).exists()) {
+            try {
+                Files.move(Path.of(backupDir), snapshotsDir.getAsFile().toPath());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private void deleteDirectory(File asFile) {
+        for (File file : asFile.listFiles()) {
+            // I want to avoid having bugs here that accidently delete outside of this directory, and
+            // I don't want dependencies, so I'll hardcode some of the directories I'm allowed to recurse
+            // into
+            if (file.isDirectory() && file.getName().equals("images")) {
+                deleteDirectory(file);
+            } else {
+                safeDelete(file);
+            }
+        }
+        safeDelete(asFile);
+    }
+
+    protected void backupDir(Directory dir) {
+        try {
+            String dest = dir.getAsFile().toString() + "-screenshotbot-backup";
+            // This isn't ideal.. but if the directory already exists, let's assume it was a previous backup, so we should restore it.
+            if (!new File(dest).exists()) {
+                Files.move(dir.getAsFile().toPath(),
+                        Path.of(dest), ATOMIC_MOVE);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
--- plugin/src/main/java/io/screenshotbot/gradle/plugin/PaparazziIntegrationBuilder.java
+++ plugin/src/main/java/io/screenshotbot/gradle/plugin/PaparazziIntegrationBuilder.java
@@ -1,17 +1,11 @@
 package io.screenshotbot.gradle.plugin;
 
-import groovy.lang.Closure;
 import org.gradle.api.*;
 import org.gradle.api.file.Directory;
 import org.gradle.api.tasks.TaskContainer;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-
-import static java.nio.file.StandardCopyOption.ATOMIC_MOVE;
 
 public class PaparazziIntegrationBuilder extends AbstractIntegrationBuilder {
     public void apply(Project project) {
@@ -92,50 +86,10 @@ public class PaparazziIntegrationBuilder extends AbstractIntegrationBuilder {
                 });
     }
 
-    private void restoreDir(Directory snapshotsDir) {
-        String backupDir = snapshotsDir.getAsFile().toString() + "-screenshotbot-backup";
-
-        deleteDirectory(snapshotsDir.getAsFile());
-
-        if (new File(backupDir).exists()) {
-            try {
-                Files.move(Path.of(backupDir), snapshotsDir.getAsFile().toPath());
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-
-    private void deleteDirectory(File asFile) {
-        for (File file : asFile.listFiles()) {
-            // I want to avoid having bugs here that accidently delete outside of this directory, and
-            // I don't want dependencies, so I'll hardcode some of the directories I'm allowed to recurse
-            // into
-            if (file.isDirectory() && file.getName().equals("images")) {
-                deleteDirectory(file);
-            } else {
-                safeDelete(file);
-            }
-        }
-        safeDelete(asFile);
-    }
-
 
     @NotNull
     private static Directory getSnapshotsDir(Project project) {
         return project.getLayout().getProjectDirectory().dir("src/test/snapshots");
     }
 
-    private void backupDir(Directory dir) {
-        try {
-            String dest = dir.getAsFile().toString() + "-screenshotbot-backup";
-            // This isn't ideal.. but if the directory already exists, let's assume it was a previous backup, so we should restore it.
-            if (!new File(dest).exists()) {
-                Files.move(dir.getAsFile().toPath(),
-                        Path.of(dest), ATOMIC_MOVE);
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
 }
