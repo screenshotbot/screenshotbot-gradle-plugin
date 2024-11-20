1909264604a72c1b1bd6ed303fb722675367fabf
Arnold Noronha <arnold@tdrhq.com>
2024-11-19 19:51:47 -0500

    Task to update commit graph, even though it's not used yet
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:

---
+++ plugin/src/main/java/io/screenshotbot/gradle/plugin/UploadCommitGraphTask.java
@@ -0,0 +1,36 @@
+package io.screenshotbot.gradle.plugin;
+
+import org.gradle.api.tasks.TaskAction;
+import org.gradle.process.ExecOperations;
+
+import javax.inject.Inject;
+import java.util.ArrayList;
+
+public class UploadCommitGraphTask extends BaseRecorderTask {
+    public void setMainBranch(String mainBranch) {
+        this.mainBranch = mainBranch;
+    }
+
+    private String mainBranch;
+
+    @Inject
+    public UploadCommitGraphTask(ExecOperations e) {
+        super(e);
+    }
+
+    @TaskAction
+    public void uploadCommitGraph() {
+        execOperations.exec((it) -> {
+            it.setExecutable((getExecutable()));
+            ArrayList<String> args = new ArrayList<>();
+            args.add("ci");
+            args.add("update-commit-graph");
+
+            if (mainBranch != null && mainBranch.length() > 0) {
+                args.add("--main-branch");
+                args.add(mainBranch);
+            }
+            it.setArgs(args);
+        })
+    }
+}
